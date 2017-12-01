17a5eb71f3b5548dc93f85aa1e0c83faa27b272e
XperiaZed <18zgriffin@mlmc.vic.edu.au>
2017-12-01 18:21:32 +1100

    Duplicated part 1 of DNA_Sequence and renamed new file to DNA_Sequence to part 2

---
+++ DNA_Sequencing Part 2.py
@@ -0,0 +1,13 @@
+#opens both of the files for reading
+sequence1 = open("suspect1.txt").read()
+frag1 = open("fragment1.txt").read()
+
+#finds the length of the fragment
+fragLen = len(frag)
+
+#finds where in the frequence the fragment is first found
+fragLoc = sequence.find(frag)
+
+#prints the location of the fragment
+print("The fragment is located from position", fragLoc, "to position", fragLen+fragLoc)
+
