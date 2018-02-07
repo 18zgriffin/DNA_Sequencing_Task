47d6a43231ca6f3f063ae0e440db6a5251607517
XperiaZed <18zgriffin@mlmc.vic.edu.au>
2018-02-08 10:24:13 +1100

    added data to all files and completed an array in the code

--- DNA_Sequencing Part 2.py
+++ DNA_Sequencing Part 2.py
@@ -11,11 +11,22 @@ sequence5 = open("suspect5.txt").read()
 frag5 = open("fragment5.txt").read()
 frag6 = open("fragment6.txt").read()
 
-#finds the length of the fragment
-fragLen = len(frag)
+#finds the length of each fragment
+fragLen1 = len(frag1)
+fragLen2 = len(frag2)
+fragLen3 = len(frag3)
+fragLen4 = len(frag4)
+fragLen5 = len(frag5)
+fragLen6 = len(frag6)
 
+
+array = [frag1, frag2, frag3, frag4, frag5, frag6]
+
+for frag in array:
+    print(frag)
+    fragLoc = sequence1.find(frag1)
+    print(fragLoc)
 #finds where in the frequence the fragment is first found
-fragLoc = sequence.find(frag)
 
 #prints the location of the fragment
 print("The fragment is located from position", fragLoc, "to position", fragLen+fragLoc)
