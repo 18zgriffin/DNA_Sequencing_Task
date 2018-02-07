4d4fc8a2021f6eae347deaf67d2bea2cb9217726
XperiaZed <18zgriffin@mlmc.vic.edu.au>
2018-02-08 10:17:07 +1100

    added all of the suspect and fragment files

--- DNA_Sequencing Part 2.py
+++ DNA_Sequencing Part 2.py
@@ -1,6 +1,15 @@
-#opens both of the files for reading
+#opens all of the files required for reading
 sequence1 = open("suspect1.txt").read()
 frag1 = open("fragment1.txt").read()
+sequence2 = open("suspect2.txt").read()
+frag2 = open("fragment2.txt").read()
+sequence3 = open("suspect3.txt").read()
+frag3 = open("fragment3.txt").read()
+sequence4 = open("suspect4.txt").read()
+frag4 = open("fragment4.txt").read()
+sequence5 = open("suspect5.txt").read()
+frag5 = open("fragment5.txt").read()
+frag6 = open("fragment6.txt").read()
 
 #finds the length of the fragment
 fragLen = len(frag)
