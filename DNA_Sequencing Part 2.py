53a6ccc74783f69f4b7fb548e288e4c7e8d22667
XperiaZed <18zgriffin@mlmc.vic.edu.au>
2018-02-08 13:43:43 +1100

    completed main for loop, tweeking required

--- DNA_Sequencing Part 2.py
+++ DNA_Sequencing Part 2.py
@@ -1,3 +1,7 @@
+#what number fragement and sequence we are up to in the loop
+fragnumber = 0
+sequnumber = 0
+
 #opens all of the files required for reading
 sequence1 = open("suspect1.txt").read()
 frag1 = open("fragment1.txt").read()
@@ -11,21 +15,20 @@ sequence5 = open("suspect5.txt").read()
 frag5 = open("fragment5.txt").read()
 frag6 = open("fragment6.txt").read()
 
-#finds the length of each fragment
-fragLen1 = len(frag1)
-fragLen2 = len(frag2)
-fragLen3 = len(frag3)
-fragLen4 = len(frag4)
-fragLen5 = len(frag5)
-fragLen6 = len(frag6)
+#creates an array with all the frags in it and an array with all the sequences in it
+fragarray = [frag1, frag2, frag3, frag4, frag5, frag6]
+sequarray = [sequence1, sequence2, sequence3, sequence4, sequence5]
 
+for frag in fragarray:
+    fragnumber += 1
+    sequnumber == 0
+    for sequence in sequarray:
+        sequnumber += 1
+        if frag in sequence:
+            fragLoc = sequence.find(frag)
+            fragLen = len(frag)
+            print("Fragment number", fragnumber, "is found in sequence", sequnumber, " from", fragLoc, "to", fragLen+fragLoc)
 
-array = [frag1, frag2, frag3, frag4, frag5, frag6]
-
-for frag in array:
-    print(frag)
-    fragLoc = sequence1.find(frag1)
-    print(fragLoc)
 #finds where in the frequence the fragment is first found
 
 #prints the location of the fragment
